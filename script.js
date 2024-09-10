c3ebc172b986f9db7c1599bbbe1b19387571896e
emirakbas <makbas1@ogr.iu.edu.tr>
2024-09-10 09:52:50 +0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Mouse arama kutusunun üzerine gelince çalışacaklar:
+
 const searchBox = document.querySelector('input');
+const searchButton = document.querySelector('button');
 searchBox.addEventListener('mouseenter', function() {
     console.log('Mouse kutunun üzerine geldi!');
     searchBox.style.width = '40%';
@@ -9,3 +11,16 @@ searchBox.addEventListener('mouseleave', function() {
     console.log('Mouse kutunun üzerinden çıktı!');
     searchBox.style.width = '20%';
 });
+
+searchButton.addEventListener('mouseenter', function() {
+    console.log('Mouse butonun üzerine geldi!');
+    searchBox.style.width = '40%';
+});
+
+searchButton.addEventListener('mouseleave', function() {
+    console.log('Mouse butonun üzerinden çıktı!');
+    searchBox.style.width = '20%';
+});
+
+// Mouse bitiş.
+
