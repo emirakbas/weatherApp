b8ff693a507d78f27d1cb2aeac0b1844090c4a83
emirakbas <makbas1@ogr.iu.edu.tr>
2024-09-10 15:58:51 +0300

    update

--- script.js
+++ script.js
@@ -2,25 +2,30 @@
 
 const searchBox = document.querySelector('input');
 const searchButton = document.querySelector('button');
+
 searchBox.addEventListener('mouseenter', function() {
     console.log('Mouse kutunun üzerine geldi!');
-    searchBox.style.width = '40%';
+    searchBox.style.width = '30em';
 });
 
 searchBox.addEventListener('mouseleave', function() {
     console.log('Mouse kutunun üzerinden çıktı!');
-    searchBox.style.width = '20%';
+    searchBox.style.width = '20em';
 });
 
 searchButton.addEventListener('mouseenter', function() {
     console.log('Mouse butonun üzerine geldi!');
-    searchBox.style.width = '40%';
+    searchBox.style.width = '30em';
 });
 
 searchButton.addEventListener('mouseleave', function() {
     console.log('Mouse butonun üzerinden çıktı!');
-    searchBox.style.width = '20%';
+    searchBox.style.width = '20em';
 });
 
 // Mouse bitiş.
+
+// API Başlangıç:
+
+
 
